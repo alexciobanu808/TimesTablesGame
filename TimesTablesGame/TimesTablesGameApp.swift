dc5645e732b4cfdae825f67b09d827a2b572c3f2
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2022-01-27 12:43:17 -0500

    Initial Commit

---
+++ TimesTablesGame/TimesTablesGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesGameApp.swift
+//  TimesTablesGame
+//
+//  Created by Alex Ciobanu on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
